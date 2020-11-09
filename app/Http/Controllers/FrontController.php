fff4d124c9dfc75b0003fc536c28d2589db3aade
dilukadewappriya <dilukadewappriya@gmail.com>
2020-11-09 15:07:29 +0530

    jijiji

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -9,12 +9,12 @@ class FrontController extends Controller
 {
         public function index()
     {
-       $first = Post::orderBy('created_at', 'desc')->first();
-       $second = Post::orderBy('created_at', 'desc')->skip(1)->take(1)->get();
-       $third = Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get();
-       $fourth = Post::orderBy('created_at', 'desc')->skip(3)->take(1)->get();
-       $fifth = Post::orderBy('created_at', 'desc')->skip(4)->take(1)->get();
-       $view= Post::orderBy('created_at', 'desc')->paginate(6);
+       $first = Post::orderBy('created_at', 'desc')->where("status",1)->first();
+       $second = Post::orderBy('created_at', 'desc')->where("status",1)->skip(1)->take(1)->get();
+       $third = Post::orderBy('created_at', 'desc')->where("status",1)->skip(2)->take(1)->get();
+       $fourth = Post::orderBy('created_at', 'desc')->where("status",1)->skip(3)->take(1)->get();
+       $fifth = Post::orderBy('created_at', 'desc')->where("status",1)->skip(4)->take(1)->get();
+       $view= Post::orderBy('created_at', 'desc')->where("status",1)->paginate(6);
         return view('frontend.postindex',compact('view','first','second','third','fourth','fifth'));
     }
 
