0c0d84ebbe9ef2ac6ae36b45d6c3e1def471d908
dilukadewappriya <dilukadewappriya@gmail.com>
2020-11-09 18:16:40 +0530

    safqfq

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,26 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
 
 class Post extends Model
 {
-    //
+    public static function postgetlist() {
+        $sql= DB::table('posts')
+        ->select('posts.*', 'users.name' , 'users.image as userimage')
+        ->leftjoin('users','posts.user_id', '=', 'users.id')
+        ->where('posts.status', '=', 1)
+        ->get();
+        return $sql;     
+    }
+
+    public static function selectpost($id) {
+        $sql= DB::table('posts')
+        ->select('posts.*', 'users.name' , 'users.image as userimage')
+        ->leftjoin('users','posts.user_id', '=', 'users.id')
+        ->where('posts.id', '=', $id)
+        ->get();
+        return $sql;     
+    }
 }
