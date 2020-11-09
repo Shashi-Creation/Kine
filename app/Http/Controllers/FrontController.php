113c0592977a0759d329a982a45708ea8b5998e0
dilukadewappriya <dilukadewappriya@gmail.com>
2020-11-09 14:30:41 +0530

    dsdasda

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -9,8 +9,13 @@ class FrontController extends Controller
 {
         public function index()
     {
+       $first = Post::orderBy('created_at', 'desc')->first();
+       $second = Post::orderBy('created_at', 'desc')->skip(1)->take(1)->get();
+       $third = Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get();
+       $fourth = Post::orderBy('created_at', 'desc')->skip(3)->take(1)->get();
+       $fifth = Post::orderBy('created_at', 'desc')->skip(4)->take(1)->get();
        $view= Post::paginate(6);
-        return view('frontend.postindex',compact('view'));
+        return view('frontend.postindex',compact('view','first','second','third','fourth','fifth'));
     }
 
 
